311814b21040e718e04a8b3ef70fcd779166be54
Артем Богдан <artembogdan@Artems-MacBook-Pro.local>
2021-07-08 08:00:05 +0300

    Added shell context processor

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from app import app
+from app.models import User, Genre, Director, Movie
+from app import db
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'user': User, 'director': Director, 'genre': Genre, 'movie': Movie}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
