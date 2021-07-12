a68b876775ef3a926a968849a09ff3987e5268d4
Артем Богдан <bogdan_artem@icloud.com>
2021-07-12 16:34:13 +0300

    Fixed user endpoint

--- app/api/users.py
+++ app/api/users.py
@@ -23,13 +23,12 @@ def get_user_movies(id):
     user = User.query.get_or_404(id)
     page = request.args.get('page', 1, type=int)
     per_page = min(request.args.get('per_page', 10, type=int), 100)
-    data = User.to_collection_dict(user.movies, page, per_page, 'api.get_ser_movies', id=id)
+    data = User.to_collection_dict(user.movies, page, per_page, 'api.get_user_movies', id=id) #TODO:Change endpoint
     return jsonify(data)
 
 
 @bp.route('/users', methods=['POST'])
 def create_user():
-    print("Before create user")
     data = request.get_json() or {}
     if 'username' not in data or 'email' not in data or 'password' not in data:
         return bad_request('Must include username, is_admin and password fields')
@@ -45,20 +44,20 @@ def create_user():
     db.session.commit()
     response = jsonify(user.to_dict())
     response.status_code = 201
-    response.headers['Location'] = url_for('api.get_user', id=user.id)
+    response.headers['Location'] = url_for('api.get_user', id=user.user_id)
     return response
 
 
 @bp.route('/users/<int:id>', methods=['PUT'])
 def update_user(id):
-    user = User.query.get_or_404(id)
+    movie = User.query.get_or_404(id)
     data = request.get_json() or {}
-    if 'username' in data and data['username'] != user.username and \
+    if 'name' in data and data['name'] != movie.username and \
             User.query.filter_by(username=data['username']).first():
         return bad_request('Please use a different username')
-    user.from_dict(data, new_user=False)
+    movie.from_dict(data, new_user=False)
     db.session.commit()
-    return jsonify(user.to_dict())
+    return jsonify(movie.to_dict())
 
 
 @bp.route('/users/<int:id>', methods=['DELETE'])
