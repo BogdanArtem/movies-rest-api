409e96e0a7ce9d6295607c90f204752f60cd77aa
Артем Богдан <bogdan_artem@icloud.com>
2021-07-12 22:46:02 +0300

    Added tokens to models

--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
 """Flask representation of models in database"""
 
 
+import os
+import base64
+from datetime import datetime, timedelta
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask import url_for
 from app import db
@@ -42,11 +45,33 @@ class User(PaginatedAPIMixin, db.Model):
     pass_hash = db.Column(db.String(128), nullable=False)
     movies = db.relationship('Movie', backref='user_added', lazy='dynamic')
 
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
+
     def set_password(self, password):
         self.pass_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.pass_hash, password)
+
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+
+    @staticmethod
+    def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return user
 
     def to_dict(self):
         data = {
