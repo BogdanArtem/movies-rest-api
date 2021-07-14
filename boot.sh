1f742818cb3c44482242ad08d973cfaa650847c3
Артем Богдан <bogdan_artem@icloud.com>
2021-07-14 08:21:08 +0300

    Added dev databse

--- boot.sh
+++ boot.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # this script is used to boot a Docker container
 pipenv run flask db init
-echo VALERA!
 pipenv run flask db upgrade
+pipenv run python3 test_database.py
 pipenv run gunicorn -w 1 -b 0.0.0.0:8000 --access-logfile - --error-logfile - main:app
